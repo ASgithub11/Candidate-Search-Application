8d37ba9074ec7c7c574f5e2b1b63e701a240d2fa
Ash <aishasiddiqa151@gmail.com>
2024-09-27 10:50:54 -0700

    Create an interface for the Candidate objects returned by the API

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,13 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    readonly id: number;
+    readonly login: string;
+    readonly avatar_url: string;
+    readonly name?: string;
+    readonly location?: string;
+    readonly email?: string;
+    readonly company?: string;
+    readonly bio?: string;
+    readonly html_Url: string;
+    declined?: boolean;
+};
