619669fd97f2b36bafd76cf9855a18124d3f1f3a
Ash <aishasiddiqa151@gmail.com>
2024-09-27 19:53:40 -0700

    Add code to display the nav bar and link between pages

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+import '../index.css';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div>
+      <nav className='nav'>
+        <Link to ="/" className='nav-link'>Home</Link>
+        <Link to ="/SavedCandidates" className='nav-link'>Potential Candidates</Link>
+      </nav>
+    </div>
   )
 };
 
