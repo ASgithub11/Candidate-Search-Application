96bed6af10e9b6efaad60c676c21bcaff20b78d2
Ash <aishasiddiqa151@gmail.com>
2024-09-28 20:25:24 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+  },
 });
